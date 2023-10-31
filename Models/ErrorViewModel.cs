f52f434fcbc950248f1b8bd0678b80c74fe187ee
Tam Ha <kingleeling@gmail.com>
2023-10-31 13:26:43 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestayrantRaterMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
